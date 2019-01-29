39832432e2965607ba7f8c0393613f72a3284c88
Tusawe <joseluisgomezlopez91@gmail.com>
2019-01-29 09:25:15 +0100

    29Ene

--- Cadenas/src/ParseoFicheros.java
+++ Cadenas/src/ParseoFicheros.java
@@ -8,27 +8,89 @@ public class ParseoFicheros {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		File inputFile = new File("Files/nombres_mujer.txt");
-		Scanner sc = new Scanner(inputFile);
-		int contador = 0;
-		String linea;
+		boolean hayArgumento = args.length !=0;
+		String dosCaracteres = "";
+		
+		if (hayArgumento && args[0].length() >= 2) {
+			
+			dosCaracteres = args[0].toLowerCase().substring(0, 2);
+			
+		}
 		
 		// Listas para almacenar datos.
 		List<String> empiezanPorA = new ArrayList<>();
 		List<String> noAcabanEnVocal = new ArrayList<>();
+		List<String> nombresMasLargos = new ArrayList<>();
+		List<String> nombresMasCortos = new ArrayList<>();
+		List<String> nombresCoincidentes = new ArrayList<>();
+		
+		File inputFile = new File("Files/nombres_mujer.txt");
+		Scanner sc = new Scanner(inputFile);
+		int contador = 0;
+		int iPalabraMayor = 0;
+		int iPalabraMenor = Integer.MAX_VALUE;
+		String linea;
 		
 		while (sc.hasNextLine()) {
 			
 			linea = sc.nextLine();
-			contador++;
-			System.out.println(linea);
+			contador++; // numero de nombres leidos.
 			
+			if (hayArgumento) {
+				
+				if (args[0].equalsIgnoreCase(linea)) {
+					
+					System.out.println("Existe el nombre " + linea);
+					return;
+					
+				}
+				
+				if (linea.toLowerCase().startsWith(dosCaracteres) && dosCaracteres.length() >= 2) {
+					
+					nombresCoincidentes.add(linea);
+					
+				}
+				
+				continue;
+				
+			}
+			
+			// Buscar nombres mas largos.
+			if (linea.length() == iPalabraMayor) {
+				
+				nombresMasLargos.add(linea);
+				// iPalabraMayor = linea.length();   NO HACE FALTA.
+				
+			} else if (linea.length() > iPalabraMayor) {
+				
+				nombresMasLargos.clear();
+				nombresMasLargos.add(linea);
+				iPalabraMayor = linea.length();
+				
+			}
+			
+			// Buscar nombres mas cortos.
+			if (linea.length() == iPalabraMenor) {
+				
+				nombresMasCortos.add(linea);
+				// iPalabraMenor = linea.length();   NO HACE FALTA.
+				
+			} else if (linea.length() < iPalabraMenor) {
+				
+				nombresMasCortos.clear();
+				nombresMasCortos.add(linea);
+				iPalabraMenor = linea.length();
+				
+			}
+			
+			// Buscar nombres que empiecen por A.
 			if (linea.matches("^[AaÁá].*")) {
 				
 				empiezanPorA.add(linea);
 				
 			}
 			
+			// Buscar nombres que no acaben en vocal.
 			if (!linea.toLowerCase().matches(".*[aeiouáéíóú]$")) {
 				
 				noAcabanEnVocal.add(linea);
@@ -37,11 +99,28 @@ public class ParseoFicheros {
 			
 		}
 		
-		System.out.println("Numero de lineas leidas: " + contador + " lineas.");
-		System.out.println("Nombres que empiezan por A -> " + empiezanPorA);
-		System.out.println("Nombres que no acaban en vocal -> " + noAcabanEnVocal);
 		sc.close();
 		
-	}
+		if (!hayArgumento) {
+		
+			System.out.println("Numero de lineas leidas: " + contador + " lineas.");
+			System.out.println("Nombres que empiezan por A -> " + empiezanPorA);
+			System.out.println("Nombres que no acaban en vocal -> " + noAcabanEnVocal);
+			System.out.println("El/los nombre/s mas largo/s es/son -> " + nombresMasLargos);
+			System.out.println("El/los nombre/s mas corto/s es/son -> " + nombresMasCortos);
+		
+		} else {
+			
+			System.out.println("No existe el nombre " + args[0]);
+			
+			if (nombresCoincidentes.size() > 0) {
+				
+				System.out.println("Nombres coincidentes -> " + nombresCoincidentes);
+			
+			}
+			
+		}
 	
+	}	
+		
 }
