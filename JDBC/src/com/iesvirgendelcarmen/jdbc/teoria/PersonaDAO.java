90b5663b6abbbef47954b190f57f0f4ab16fc20c
Tusawe <joseluisgomezlopez91@gmail.com>
2019-03-29 11:54:51 +0100

    29Mar

--- JDBC/src/com/iesvirgendelcarmen/jdbc/teoria/PersonaDAO.java
+++ JDBC/src/com/iesvirgendelcarmen/jdbc/teoria/PersonaDAO.java
@@ -2,9 +2,11 @@ package com.iesvirgendelcarmen.jdbc.teoria;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonaDAO {
@@ -21,7 +23,7 @@ public class PersonaDAO {
 	
 	public List<Persona> obtenerTodasLasPersonas() {
 		
-		List<Persona> lista = null;
+		List<Persona> lista = new ArrayList<>();
 		
 		String sql = "SELECT * FROM persona";
 		try(Statement sentencia = conexion.createStatement();
@@ -44,7 +46,41 @@ public class PersonaDAO {
 	
 	public Persona obtenerPersonaPorEmail(String email) {
 		
-		return null;
+		String sql = "SELECT * FROM persona WHERE email = ?;";
+		Persona persona = new Persona();
+		try(PreparedStatement preStatement = conexion.prepareStatement(sql);) {
+			preStatement.setString(1, email);
+			ResultSet rsSet = preStatement.executeQuery();
+			while(rsSet.next()) {
+				
+				persona = new Persona(rsSet.getInt("id"),
+						rsSet.getString("first_name"),
+						rsSet.getString("last_name"),
+						rsSet.getString("email"));
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return persona;
+		
+	}
+	
+	public boolean borrarPersona(String email) {
+		
+		String sql = "DELETE FROM persona WHERE email = ?;";
+		int rsSet = 0;
+		try(PreparedStatement preStatement = conexion.prepareStatement(sql);) {
+			preStatement.setString(1, email);
+			rsSet = preStatement.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rsSet != 0;
 		
 	}
 	
--- JDBC/src/com/iesvirgendelcarmen/jdbc/teoria/tests/PersonaDAOTest.java
+++ JDBC/src/com/iesvirgendelcarmen/jdbc/teoria/tests/PersonaDAOTest.java
@@ -9,21 +9,31 @@ import com.iesvirgendelcarmen.jdbc.teoria.PersonaDAO;
 
 public class PersonaDAOTest {
 
-	@Test
-	public void testObtenerTodasLasPersonas() {
-		PersonaDAO personaDAO = new PersonaDAO();
-		assertTrue(personaDAO.obtenerTodasLasPersonas().size() == 1000);
-		assertTrue(personaDAO.obtenerTodasLasPersonas().get(999)
-				.equals(new Persona("Lishe","Ashe")));
-	}
+//	@Test
+//	public void testObtenerTodasLasPersonas() {
+//		PersonaDAO personaDAO = new PersonaDAO();
+//		assertTrue(personaDAO.obtenerTodasLasPersonas().size() == 1000);
+//		assertTrue(personaDAO.obtenerTodasLasPersonas().get(999)
+//				.equals(new Persona("Lishe","Ashe")));
+//	}
 
+//	@Test
+//	public void testObtenerPersonaPorEmail() {
+//		PersonaDAO personaDAO = new PersonaDAO();
+//		assertTrue(personaDAO.obtenerPersonaPorEmail("lasherr@jalbum.net")
+//				.equals(new Persona("Lishe","Ashe")));
+//		assertFalse(personaDAO.obtenerPersonaPorEmail("lasherr@jalbum.net")
+//				.equals(new Persona("Jabez","Virgoe")));
+//	}
+	
 	@Test
-	public void testObtenerPersonaPorEmail() {
+	public void testBorrarPersona() {
+		
 		PersonaDAO personaDAO = new PersonaDAO();
-		assertTrue(personaDAO.obtenerPersonaPorEmail("lasherr@jalbum.net")
-				.equals(new Persona("Lishe","Ashe")));
-		assertFalse(personaDAO.obtenerPersonaPorEmail("lasherr@jalbum.net")
+		assertTrue(personaDAO.obtenerPersonaPorEmail("jvirgoerq@woothemes.com")
 				.equals(new Persona("Jabez","Virgoe")));
+		assertTrue(personaDAO.borrarPersona("jvirgoerq@woothemes.com"));
+		
 	}
 
 }
