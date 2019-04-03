db176d650253e35b4cbd69288a44d1fbb8a5eab4
Tusawe <joseluisgomezlopez91@gmail.com>
2019-04-03 10:12:30 +0200

    03Abr

--- Formularios/src/forms/Gui.java
+++ Formularios/src/forms/Gui.java
@@ -4,14 +4,11 @@ public class Gui {
 
 	public static void main(String[] args) {
 		
-//		SecondForm segundo = new SecondForm();
-//		segundo.setVisible(true);
+//		Plantilla plantilla = new Plantilla();
+//		plantilla.setVisible(true);
 		
 		FirstForm primero = new FirstForm();
 		primero.setVisible(true);
-//		
-//		Plantilla plantilla = new Plantilla();
-//		plantilla.setVisible(true);
 
 	}
 
