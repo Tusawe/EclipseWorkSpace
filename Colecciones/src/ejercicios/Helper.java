059bfeeb99b5b993f7be010bb3552beaac8abb1b
Tusawe <joseluisgomezlopez91@gmail.com>
2019-04-08 12:45:31 +0200

    08Abr_12:45

--- Colecciones/src/ejercicios/Helper.java
+++ Colecciones/src/ejercicios/Helper.java
@@ -1,5 +1,39 @@
 package ejercicios;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
 public class Helper {
 
+	public static Map<String,String[]> getDataFromFile(File file) throws FileNotFoundException {
+		
+		Gson gson = new Gson();
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		Car[] cars = gson.fromJson(in, Car[].class);
+		
+		for (Car car : cars) {
+			System.out.println(car.getBrand() + ", " + 
+					car.getModel() + ", " + car.getPlate() + ".");
+		}
+		
+		return null;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			getDataFromFile(new File("data/car.json"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
